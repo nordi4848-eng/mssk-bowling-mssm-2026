6d9fbb40cf980e262c0ba30fb5cfa96ffd77403d
nordi4848-eng <nordi4848@gmail.com>
2026-08-31 15:13:33 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mssk-bowling-2026-v5-7-11';
+const CACHE_NAME = 'mssk-bowling-2026-v5-7-12';
 const CORE = ['./manifest.json','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 
 self.addEventListener('install', event => {
